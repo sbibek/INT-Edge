56549ff7cfb87e2027b310e0f7386ea8b25380c3
Chameleon Cloud User <cc@u64.novalocal>
2020-11-13 06:34:11 +0000

    changes

--- vm/root-bootstrap.sh
+++ vm/root-bootstrap.sh
@@ -3,15 +3,9 @@
 # Print commands and exit on errors
 set -xe
 
-DEBIAN_FRONTEND=noninteractive sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-DEBIAN_FRONTEND=noninteractive sudo add-apt-repository -y ppa:webupd8team/atom
-
-apt-get update
-
 KERNEL=$(uname -r)
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 apt-get install -y --no-install-recommends --fix-missing\
-  atom \
   autoconf \
   automake \
   bison \
@@ -20,12 +14,11 @@ apt-get install -y --no-install-recommends --fix-missing\
   cmake \
   cpp \
   curl \
-  emacs24 \
   flex \
   git \
   libboost-dev \
   libboost-filesystem-dev \
-  libboost-iostreams1.58-dev \
+  libboost-all-dev \
   libboost-program-options-dev \
   libboost-system-dev \
   libboost-test-dev \
@@ -66,27 +59,3 @@ apt-get install -y --no-install-recommends --fix-missing\
   xcscope-el \
   xterm
 
-useradd -m -d /home/p4 -s /bin/bash p4
-echo "p4:p4" | chpasswd
-echo "p4 ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/99_p4
-chmod 440 /etc/sudoers.d/99_p4
-usermod -aG vboxsf p4
-
-cd /usr/share/lubuntu/wallpapers/
-cp /home/vagrant/p4-logo.png .
-rm lubuntu-default-wallpaper.png
-ln -s p4-logo.png lubuntu-default-wallpaper.png
-rm /home/vagrant/p4-logo.png
-cd ~
-sed -i s@#background=@background=/usr/share/lubuntu/wallpapers/1604-lubuntu-default-wallpaper.png@ /etc/lightdm/lightdm-gtk-greeter.conf
-
-# Disable screensaver
-apt-get -y remove light-locker
-
-# Automatically log into the P4 user
-cat << EOF | tee -a /etc/lightdm/lightdm.conf.d/10-lightdm.conf
-[SeatDefaults]
-autologin-user=p4
-autologin-user-timeout=0
-user-session=Lubuntu
-EOF
