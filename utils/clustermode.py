8e3147ae8d5f6ae4c46ceaba03fdcc28629d92b4
sbibek <sbibek@hotmail.com>
2020-12-09 19:29:11 +0000

    refactors-1

--- utils/clustermode.py
+++
@@ -1,6 +0,0 @@
-from p4_mininet import P4Switch, P4Host
-from p4runtime_switch import P4RuntimeSwitch
-from mininet.examples.cluster import RemoteMixin
-
-class RemoteP4Host(RemoteMixin, P4Host):
-    pass
