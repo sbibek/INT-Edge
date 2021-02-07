274becbf42bd2071d9eb90d260c62bebb207b7d1
sbibek <sbibek@hotmail.com>
2021-02-07 11:31:14 -0800

    bw changes

--- modules/metrics/setbw.sh
+++ modules/metrics/setbw.sh
@@ -46,10 +46,10 @@ sudo tc qdisc add dev s3-eth1 root netem delay 10ms rate 5mbit
 sudo tc qdisc add dev s3-eth2 root netem delay 10ms rate 5mbit
 sudo tc qdisc add dev s4-eth1 root netem delay 10ms rate 8mbit
 sudo tc qdisc add dev s4-eth2 root netem delay 10ms rate 8mbit
-sudo tc qdisc add dev s9-eth1 root netem delay 10ms rate 8mbit
-sudo tc qdisc add dev s9-eth2 root netem delay 10ms rate 8mbit
-sudo tc qdisc add dev s9-eth3 root netem delay 10ms rate 8mbit
-sudo tc qdisc add dev s9-eth4 root netem delay 10ms rate 8mbit
+sudo tc qdisc add dev s9-eth1 root netem delay 10ms rate 6mbit
+sudo tc qdisc add dev s9-eth2 root netem delay 10ms rate 6mbit
+sudo tc qdisc add dev s9-eth3 root netem delay 10ms rate 6mbit
+sudo tc qdisc add dev s9-eth4 root netem delay 10ms rate 6mbit
 
 sudo tc qdisc del dev s11-eth1 root
 sudo tc qdisc del dev s11-eth2 root
@@ -59,10 +59,10 @@ sudo tc qdisc del dev s10-eth1 root
 sudo tc qdisc del dev s10-eth2 root
 sudo tc qdisc del dev s10-eth3 root
 sudo tc qdisc del dev s10-eth4 root
-sudo tc qdisc add dev s11-eth1 root netem delay 10ms rate 5mbit
-sudo tc qdisc add dev s11-eth2 root netem delay 10ms rate 5mbit
-sudo tc qdisc add dev s12-eth1 root netem delay 10ms rate 5mbit
-sudo tc qdisc add dev s12-eth2 root netem delay 10ms rate 5mbit
+sudo tc qdisc add dev s11-eth1 root netem delay 10ms rate 8mbit
+sudo tc qdisc add dev s11-eth2 root netem delay 10ms rate 8mbit
+sudo tc qdisc add dev s12-eth1 root netem delay 10ms rate 8mbit
+sudo tc qdisc add dev s12-eth2 root netem delay 10ms rate 8mbit
 sudo tc qdisc add dev s10-eth1 root netem delay 10ms rate 8mbit
 sudo tc qdisc add dev s10-eth2 root netem delay 10ms rate 8mbit
 sudo tc qdisc add dev s10-eth3 root netem delay 10ms rate 8mbit
